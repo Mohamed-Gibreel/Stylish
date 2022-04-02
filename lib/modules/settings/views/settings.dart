5faf4fb505341d764991b409385d69f94c7effae
Mohamed Gibreel <m.adilgibreel@gmail.com>
2022-04-02 19:16:48 +0400

    Changed settings items to inkwell

--- lib/modules/settings/views/settings.dart
+++ lib/modules/settings/views/settings.dart
@@ -38,55 +38,62 @@ class _SettingsScreenState extends State<SettingsScreen> {
       SettingsItem('Update', CupertinoIcons.checkmark_seal, isRadio: true);
 
   Widget _settingsButton(SettingsItem item) {
-    return GestureDetector(
-      behavior: HitTestBehavior.opaque,
-      onTap: item.isRadio
-          ? () {
-              item.toggleValue();
-              if (mounted) setState(() {});
-            }
-          : null,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 42.w,
-                height: 42.h,
-                margin: EdgeInsets.only(right: 17.w),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFF67952).withOpacity(.07),
-                  borderRadius: BorderRadius.circular(10.r),
-                ),
-                child: Icon(
-                  item.icon,
-                  color: const Color(0xFFF67952),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 5.h),
-                child: Text(
-                  item.name,
-                  style: TextStyle(fontSize: 14.sp),
-                ),
-              ),
-            ],
-          ),
-          if (!item.isRadio)
-            const Icon(
-              Icons.arrow_forward_ios_rounded,
-            ),
-          if (item.isRadio)
-            Switch.adaptive(
-              value: item.value,
-              onChanged: (_) {
+    return Material(
+      child: InkWell(
+        onTap: item.isRadio
+            ? () {
                 item.toggleValue();
                 if (mounted) setState(() {});
-              },
-              activeColor: Constants.primaryColor,
-            )
-        ],
+              }
+            : () {},
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: SizedBox(
+            height: 65.h,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 42.w,
+                      height: 42.h,
+                      margin: EdgeInsets.only(right: 17.w),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFF67952).withOpacity(.07),
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
+                      child: Icon(
+                        item.icon,
+                        color: const Color(0xFFF67952),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5.h),
+                      child: Text(
+                        item.name,
+                        style: TextStyle(fontSize: 14.sp),
+                      ),
+                    ),
+                  ],
+                ),
+                if (!item.isRadio)
+                  const Icon(
+                    Icons.arrow_forward_ios_rounded,
+                  ),
+                if (item.isRadio)
+                  Switch.adaptive(
+                    value: item.value,
+                    onChanged: (_) {
+                      item.toggleValue();
+                      if (mounted) setState(() {});
+                    },
+                    activeColor: Constants.primaryColor,
+                  )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -105,35 +112,32 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ),
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 36.h,
-            ),
-            _settingsButton(email),
-            SizedBox(
-              height: 25.h,
-            ),
-            _settingsButton(faq),
-            SizedBox(
-              height: 25.h,
-            ),
-            _settingsButton(privacy),
-            SizedBox(
-              height: 25.h,
-            ),
-            _settingsButton(notification),
-            SizedBox(
-              height: 25.h,
-            ),
-            _settingsButton(update),
-            SizedBox(
-              height: 25.h,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 36.h,
+          ),
+          _settingsButton(email),
+          // SizedBox(
+          //   height: 25.h,
+          // ),
+          _settingsButton(faq),
+          // SizedBox(
+          //   height: 25.h,
+          // ),
+          _settingsButton(privacy),
+          // SizedBox(
+          //   height: 25.h,
+          // ),
+          _settingsButton(notification),
+          // SizedBox(
+          //   height: 25.h,
+          // ),
+          _settingsButton(update),
+          // SizedBox(
+          //   height: 25.h,
+          // ),
+        ],
       ),
     );
   }
